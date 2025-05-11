76ef58bfeac46adbd53d54fba4296c39809e98e6
Javohir Home Desktop <javoxirone@gmail.com>
2025-05-11 17:41:57 +0500

    added documentation for services

--- app/services/tournament.py
+++ app/services/tournament.py
@@ -14,6 +14,15 @@ from app.exceptions.tournament import (
 
 
 def create_tournament(data: TournamentInDBInput) -> TournamentInDBOutput:
+    """
+    This service creates a new tournament in the database and returns tournament data.
+
+    :param data: Input data for the new tournament.
+    :type data: TournamentInDBInput
+
+    :return: Tournament data for the newly created tournament.
+    :rtype: TournamentInDBOutput
+    """
     tournament_repo = TournamentRepo()
     try:
         tournament = tournament_repo.create_tournament(data)
@@ -27,6 +36,15 @@ def create_tournament(data: TournamentInDBInput) -> TournamentInDBOutput:
 
 
 def get_tournament(tournament_id: int) -> TournamentInDBOutput:
+    """
+    Fetches a tournament from the database by its ID and handles exceptions.
+
+    :param tournament_id: The ID of the tournament to fetch.
+    :type tournament_id: int
+
+    :return: The fetched tournament data.
+    :rtype: TournamentInDBOutput
+    """
     tournament_repo = TournamentRepo()
     try:
         tournament = tournament_repo.get_tournament(tournament_id)
@@ -40,6 +58,12 @@ def get_tournament(tournament_id: int) -> TournamentInDBOutput:
 
 
 def get_tournaments() -> list[TournamentInDBOutput]:
+    """
+    Fetches a list of tournaments from the repository.
+
+    :return: A list of tournament data.
+    :rtype: list[TournamentInDBOutput]
+    """
     tournament_repo = TournamentRepo()
     try:
         tournaments = tournament_repo.get_tournaments()
@@ -53,6 +77,18 @@ def get_tournaments() -> list[TournamentInDBOutput]:
 def update_tournament(
     tournament_id: int, data: TournamentInDBInput
 ) -> TournamentInDBOutput:
+    """
+    Updated the existing tournament data on the database.
+
+    :param tournament_id: The ID of the tournament to update.
+    :type tournament_id: int
+
+    :param data: The updated tournament data.
+    :type data: TournamentInDBInput
+
+    :return: The updated tournament data.
+    :rtype: TournamentInDBOutput
+    """
     tournament_repo = TournamentRepo()
     try:
         updated_tournament = tournament_repo.update_tournament(tournament_id, data)
@@ -68,6 +104,15 @@ def update_tournament(
 
 
 def delete_tournament(tournament_id: int) -> bool:
+    """
+    Deletes a tournament from the database.
+
+    :param tournament_id: The ID of the tournament to delete.
+    :type tournament_id: int
+
+    :return: True if the tournament was deleted successfully, False otherwise.
+    :rtype: bool
+    """
     tournament_repo = TournamentRepo()
     try:
         return tournament_repo.delete_tournament(tournament_id)
